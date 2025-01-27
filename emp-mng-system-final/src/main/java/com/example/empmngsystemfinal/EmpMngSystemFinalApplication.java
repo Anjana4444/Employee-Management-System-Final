1e42d352f241dfbb5fb0808548f7a49527405e69
Anjana <aaluwihare4@gmail.com>
2025-01-27 13:53:31 +0530

    base

---
+++ emp-mng-system-final/src/main/java/com/example/empmngsystemfinal/EmpMngSystemFinalApplication.java
@@ -0,0 +1,13 @@
+package com.example.empmngsystemfinal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmpMngSystemFinalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmpMngSystemFinalApplication.class, args);
+	}
+
+}
---
+++ emp-mng-system-final/src/test/java/com/example/empmngsystemfinal/EmpMngSystemFinalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.empmngsystemfinal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmpMngSystemFinalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
