6fe41fd182e0c14d2dd2a90b47f05534846d34e4
Kasun Gayan <kasungayan333@gmail.com>
2025-01-27 14:08:57 +0530

    Database

---
+++ emp-mng-system-final/src/main/java/com/example/empmngsystemfinal/entity/employee.java
@@ -0,0 +1,56 @@
+package com.example.empmngsystemfinal.entity;
+
+import com.fasterxml.jackson.annotation.JsonTypeId;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class employee {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private long id;
+    private String firstname;
+    private String lastname;
+    private String email;
+
+    public employee() {
+    }
+
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getFirstname() {
+        return firstname;
+    }
+
+    public void setFirstname(String firstname) {
+        this.firstname = firstname;
+    }
+
+    public String getLastname() {
+        return lastname;
+    }
+
+    public void setLastname(String lastname) {
+        this.lastname = lastname;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+
+
+}
