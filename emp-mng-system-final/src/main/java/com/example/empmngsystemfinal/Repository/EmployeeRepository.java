1820e69f8a8d0fdbcf86587707fa35e70ac78067
Kasun Gayan <155733907+Kasun09@users.noreply.github.com>
2025-01-30 20:59:38 +0530

    Delete emp-mng-system-final directory

--- emp-mng-system-final/src/main/java/com/example/empmngsystemfinal/Repository/EmployeeRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.empmngsystemfinal.Repository;
-
-import com.example.empmngsystemfinal.entity.Employee;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-
-public interface EmployeeRepository extends JpaRepository<Employee,Long> {
-}
